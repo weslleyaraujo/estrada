7b990a94d85cf748619c8fe3e187fe6162a2ff1a
Weslley Araujo <weslleyaraujo20@gmail.com>
2015-03-28 22:59:51 -0300

    Feature: Creating example, single file

--- app.js
+++ app.js
@@ -18,12 +18,22 @@
   App.prototype.register = function (options) {
     Object.keys(options.routes).forEach(function (item, index) {
       this.routes[item] = {
-        callback: options[options.routes[item]],
+        callback: this.getCallback(options[options.routes[item]]),
         match: this.createMatch(item)
       };
     }.bind(this))
 
     return this;
+  };
+
+  App.prototype.getCallback = function (fn) {
+    return typeof fn === 'function' ? fn : function () {
+      console.log('[router]: callback not found for this route!');
+    };
+  };
+
+  App.prototype.not = function (fn) {
+    return typeof fn === 'function' ? fn : function () { console.log('yea');};
   };
 
   App.prototype.getParameters = function (route) {
@@ -40,14 +50,19 @@
   };
 
   App.prototype.createMatch = function (route) {
-    if (route === "") {
+    route = this.prepareRoute(route);
+    if (route === "/") {
       return new RegExp(/^\s*$/);
     }
 
     return route.split('/').map(function (item) {
       return !!item.match(/:/) ? "[^\\/]*" : item;
     }).join('\\/') + '$';
-  },
+  };
+
+  App.prototype.prepareRoute = function (route) {
+    return route.charAt(0) === '/' ? route : '/' + route; 
+  };
 
   App.prototype.start = function () {
     var actual, bawords;
@@ -77,36 +92,3 @@
   root.App = new App();
 
 } (window, window.addEventListener));
-
-
-App.register({
-  routes: {
-    '': 'foo',
-    '/page/:id': 'singlePage',
-    '/page/:id/teste/:seila': 'page',
-    '/bar/:id': 'bar',
-    '/teste/:id/yea': 'yea'
-  },
-
-  yea: function (id) {
-    console.log('uhul', id);
-  },
-
-  singlePage: function () {
-    console.log('single');
-  },
-
-  foo: function () {
-    console.log('foo');
-  },
-
-  page: function () {
-    console.log(arguments);
-  },
-
-  bar: function () {
-    console.log('thats bar');
-  }
-});
-
-App.start();
